2af1377a854835876d84262505fb0f0b779daa4d
girikrish332 <girikrish332@gmail.com>
2023-09-19 16:57:15 +0530

    Update TATA_SIP_UP.py

--- TATA_SIP_UP.py
+++ TATA_SIP_UP.py
@@ -5,7 +5,7 @@ from email.message import EmailMessage
 try: 
     smtp = smtplib.SMTP('smtp.gmail.com', 587) 
     smtp.starttls() 
-    smtp.login("ggdharan2413@gmail.com","GlassTumbler")
+    smtp.login("ggdharan2413@gmail.com","")
     d = datetime.now()
     message = 'Subject: {}\n\n{}\n{}\n{}'.format('Alert !. BSNL SIP UP %s' %d, 'Need action. BSNL SIP is UP from Puducherry Elderline SIP Server.','Thanks and Regards,','Puducherry Elderline')
     smtp.sendmail("ggdharan2413@gmail.com", "girikrish332@gmail.com",message) 
